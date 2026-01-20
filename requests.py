02a31d2b3c86406afc2eac41c8f2814b1ee4fa6f
Sayer Harry <sayerh2005@gmail.com>
2026-01-19 21:11:24 -0800

    found appropriate API for queries

--- requests.py
+++ requests.py
@@ -3,6 +3,8 @@ import pandas as pd
 
 #Search request handler
 #Access api and return result
+#https://rapidapi.com/apiheya/api/tripadvisor16
+#returns all tickets within criteria as well as all vendors. Only add best vendor link to table
 
 url = "url"
 data = ""
