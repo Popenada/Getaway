9c6ab57d3e8b5df815c977ee30748107df4460e6
Sayer Harry <sayerh2005@gmail.com>
2026-02-03 19:08:05 -0800

    added example url to routes, begun testing post version of api calls

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,5 @@
 from app import app
+from flask import request
 
 @app.route('/')
 @app.route('/index')
@@ -8,4 +9,23 @@ def index():
 
 @app.route('/api/flight-search', methods=["GET"])
 def flightSearch():
-    return "hello world!"
+    
+    # api request function
+    # pulls parameters from url and echoes them back
+    # Example url: http://localhost:5000/api/flight-search?searchid=test&departure=LAX
+    # type 'flask run' into terminal and go to url to test
+    
+    searchid = request.args.get("searchid") # unique identifier for search session
+    departureDate = request.args.get("departure") # Departure date formatted asw YYYY-MM-DD
+    destination = request.args.get("destination") # Destination Airport code XYZ
+    returnDate = request.args.get("return") # Return date formatted as YYY-MM-DD
+    origin = request.args.get("origin") # Origin Airport cose ABC
+    adults = request.args.get("adults") # Number of adults, int
+    
+    return {
+        "searchid" : searchid,
+        "origin": origin,
+        "destination": destination,
+        "departure": departureDate,
+        "return": returnDate,
+        "numAdults": adults}
--- get_tickets.py
+++ get_tickets.py
@@ -1,7 +1,7 @@
 import json
 from amadeus import Client, Location, ResponseError
 
-def query(origin, dest, dDate, numAdults):
+def query(searchid, origin, destination, dDate, rDate, numAdults):
     # origin & dest = "XYZ" airport codes.
     # Departure date formatted as "YYYY-MM-DD"
     # numAdults is int formatted as string "X"
@@ -11,15 +11,93 @@ def query(origin, dest, dDate, numAdults):
         client_secret='nKCQ8UrGPVORjjIa'
     )
     try:
+
         response = amadeus.shopping.flight_offers_search.get(
             originLocationCode = origin,
-            destinationLocationCode=dest,
+            destinationLocationCode=destination,
             departureDate = dDate,
-            adults = numAdults
+            adults = numAdults,
+            returnDate = rDate
         )
-        with open("new_example_response.json", "w") as file:
+        '''
+        parameters = {
+            "currencyCode": "USD",
+            "originDestinations": [ {
+                "id": "1", "originLocationCode": origin,
+                "destinationLocationCode": destination, "departureDateTimeRange": {
+                    "date": dDate, "time": "00:00:00"  
+            } }, {
+                "id": "2",
+                "originLocationCode": destination,
+                "destinationLocationCode": origin,  
+                "departureDateTimeRange": { 
+                    "date": rDate, 
+                    "time": "00:00:00" 
+                    } } ],
+            "travelers": [ { 
+                "id":  "1", 
+                "travelerType": "ADULT" 
+            }, { 
+                "id": "2",
+                "travelerType": "ADULT" \
+            }, { 
+                "id": "3",
+                "travelerType": "HELD_INFANT", 
+                "associatedAdultId": "1" 
+                } ], 
+            "sources": [ "GDS" ],
+            "searchCriteria": {  
+            "excludeAllotments": False,
+            "addOneWayOffers": True,
+            "maxFlightOffers": 10,  
+            "allowAlternativeFareOptions": True,
+            "oneFlightOfferPerDay": False, 
+            "additionalInformation": { 
+                "chargeableCheckedBags": False, 
+                "brandedFares": True, 
+                "fareRules": False 
+            },
+            "pricingOptions": { 
+                "includedCheckedBagsOnly": True 
+                }, 
+            "flightFilters": { 
+                "crossBorderAllowed": True,
+                "moreOvernightsAllowed": True,
+                "returnToDepartureAirport": True,
+                "railSegmentAllowed": True,
+                "busSegmentAllowed": True,
+                "carrierRestrictions": { 
+                    "blacklistedInEUAllowed": True,
+                    "includedCarrierCodes": [ "FA" ]
+                    }, 
+                "cabinRestrictions": [ 
+                    { 
+                        "cabin": "ECONOMY",
+                        "coverage": "MOST_SEGMENTS",
+                        "originDestinationIds": [ "2" ] 
+                    }, { 
+                        "cabin": "ECONOMY",
+                        "coverage": "MOST_SEGMENTS",
+                        "originDestinationIds": [ "1" ] 
+                    } ],
+                "connectionRestriction": { 
+                    "airportChangeAllowed": True,
+                    "technicalStopsAllowed": True 
+            } } } }
+
+    
+        response = amadeus.shopping.flight_offers_search.post(json.dumps(parameters))
+        
+        with open("post_response.json", "w") as file:
+            file.write(json.dumps(response.result, indent=4))
+        '''
+        
+        f = "%s.json" %searchid
+        with open(f, "w") as file:
             file.write(json.dumps(response.result, indent=4))
     except ResponseError as error:
         print(error)
-        
-query("LAX", "DAL", "2026-02-13", "1")
+
+
+#debug
+query("TEST-SEARCH", "LAX", "DAL", "2026-02-13", "2026-02-20", "1")
