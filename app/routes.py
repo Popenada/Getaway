93051a5d766686ff83ce5d8b0741f6285fd651fd
Sayer Harry <67023793+SayerHarry@users.noreply.github.com>
2026-02-24 20:12:52 -0800

    Fixed input key typo for maxPrice

--- app/routes.py
+++ app/routes.py
@@ -26,15 +26,15 @@ def flightSearch():
     inputs = {
         "searchid" : body.get("searchid", request.args.get("TEST-ID")),
         "departureDate" : tuple(body.get("departureDate", request.args.get("departureDate"))), # tuple of Departure dates, formatted asw YYYY-MM-DD
-        "destinations" : body.get("arrivalLocation", request.args.get("arrivalLocation")), # list of Destination Airport codes "XYZ"
+        "destinations" : body.get("arrivalCode", request.args.get("arrivalCode")), # list of Destination Airport codes "XYZ"
         "returnDate" : tuple(body.get("returnDate", request.args.get("returnDate"))), # tuple of Return dates, formatted as YYYY-MM-DD
-        "origins" : body.get("departureLocation", request.args.get("departureLocation")), # list of Origin Airport codes "ABC"
+        "origins" : body.get("departureCode", request.args.get("departureCode")), # list of Origin Airport codes "ABC"
         "adults" : int(body.get("travelers", request.args.get("travelers", 1))), # Number of adults, int}
         "roundTrip" : body.get("roundTrip", request.args.get("roundTrip", "True")) in ("true", "True", "TRUE"), #bool, true = round trip
         "nonstop" : body.get("nonStop", request.args.get("nonStop")) in ("true", "True", "TRUE"), #bool, whether or not there are connecting flights
         "included" : body.get("includedAirline", request.args.get("includedAirline")), #list of strings, list contains allowed airline codes
         "excluded" : body.get(request.args.get("excludedAirline")), #list of strings, list contains excluded airline codes
-        "maxPrice" : int(body.get("excludedAirline", request.args.get("maxPrice", 0))), #int, maximum allowed price
+        "maxPrice" : int(body.get("maxPrice", request.args.get("maxPrice", 0))), #int, maximum allowed price
         "tripLength" : int(body.get(request.args.get("tripLength",0))), #int, max number of days between departure and arrival
         "departureWindow" : int(body.get("tripLength", request.args.get("departureWindow",0))), #int, range of dates near selected departure, max 3
         "returnWindow" : int(body.get("returnWindow", request.args.get("returnWindow",0))) #int, range of dates near selected arrival, max 3
