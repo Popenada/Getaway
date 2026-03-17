986905b4ec19f9c9f451e95473096639f7d60cda
Sayer Harry <sayerh2005@gmail.com>
2026-03-16 20:32:18 -0700

    updated release summary

--- run.py
+++ run.py
@@ -2,8 +2,8 @@ from app import app
 from waitress import serve
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    #app.run(debug=True, host='0.0.0.0')
  
     #Use waitress to serve endpoint for better stability
-    # serve(app, host='0.0.0.0', port=5000)
+    serve(app, host='0.0.0.0', port=5000)
 
